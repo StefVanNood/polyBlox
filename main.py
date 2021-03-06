86c2ac89007ea3d04bd1515f57da67a92349fd48
StefVanNood <vannoodstef@gmail.com>
2021-03-06 16:16:09 +0100

    render2

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ cameraX = 0
 cameraY = 0
 vel = 5
 
+
+def vertices(x, y, z):
+    pygame.draw.polygon(win, (255, 0, 0),
+                        ((120 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                         (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                         (100 + (cameraX / 1.5), 100 + (cameraY / 1.5))))
+    pygame.draw.polygon(win, (0, 0, 255),
+                        ((100 + cameraX, 100 + cameraY),
+                         (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                         (300 + cameraX, 100 + cameraY)))
+
+
 run = True
 while run:
     win.fill((0, 0, 0))
@@ -19,27 +31,69 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         cameraX = cameraX - vel
+        print(cameraX)
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         cameraX = cameraX + vel
+        print(cameraX)
     if keys[pygame.K_w] or keys[pygame.K_UP]:
         cameraY = cameraY - vel
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         cameraY = cameraY + vel
 
-    pygame.draw.line(win, (255, 255, 255), (100 + cameraX, 100), (300 + cameraX, 100))
-    pygame.draw.line(win, (255, 255, 255), (100 + cameraX, 100), (100 + cameraX, 300))
-    pygame.draw.line(win, (255, 255, 255), (100 + cameraX, 300), (300 + cameraX, 300))
-    pygame.draw.line(win, (255, 255, 255), (300 + cameraX, 300), (300 + cameraX, 100))
-
-    pygame.draw.line(win, (255, 255, 255), (100 + cameraX, 100), (120 + (cameraX / 1.5), 120))
-    pygame.draw.line(win, (255, 255, 255), (100 + cameraX, 300), (120 + (cameraX / 1.5), 280))
-    pygame.draw.line(win, (255, 255, 255), (300 + cameraX, 300), (280 + (cameraX / 1.5), 280))
-    pygame.draw.line(win, (255, 255, 255), (300 + cameraX, 100), (280 + (cameraX / 1.5), 120))
-
-    pygame.draw.line(win, (255, 255, 255), (120 + (cameraX / 1.5), 120), (280 + (cameraX / 1.5), 120))
-    pygame.draw.line(win, (255, 255, 255), (120 + (cameraX / 1.5), 120), (120 + (cameraX / 1.5), 280))
-    pygame.draw.line(win, (255, 255, 255), (120 + (cameraX / 1.5), 280), (280 + (cameraX / 1.5), 280))
-    pygame.draw.line(win, (255, 255, 255), (280 + (cameraX / 1.5), 280), (280 + (cameraX / 1.5), 120))
+    #top
+    if cameraY >= 60:
+        pygame.draw.polygon(win, (255, 0, 0),
+                            ((120 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (100 + cameraX, 100 + cameraY)))
+        pygame.draw.polygon(win, (0, 0, 255),
+                            ((100 + cameraX, 100 + cameraY),
+                            (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (300 + cameraX, 100 + cameraY)))
+    #top end
+    #bottom
+    if cameraY <= -60:
+        pygame.draw.polygon(win, (255, 0, 0),
+                            ((120 + (cameraX / 1.5), 280 + (cameraY / 1.5)),
+                            (280 + (cameraX / 1.5), 280 + (cameraY / 1.5)),
+                            (100 + cameraX, 300 + cameraY)))
+        pygame.draw.polygon(win, (0, 0, 255),
+                            ((100 + cameraX, 300 + cameraY),
+                            (280 + (cameraX / 1.5), 280 + (cameraY / 1.5)),
+                            (300 + cameraX, 300 + cameraY)))
+    #bottom end
+    #left
+    if cameraX >= 60:
+        pygame.draw.polygon(win, (255, 0, 0),
+                            ((120 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (100 + cameraX, 100 + cameraY),
+                            (120 + (cameraX / 1.5), 280 + (cameraY / 1.5))))
+        pygame.draw.polygon(win, (0, 0, 255),
+                            ((120 + (cameraX / 1.5), 280 + (cameraY / 1.5)),
+                            (100 + cameraX, 100 + cameraY),
+                            (100 + cameraX, 300 + cameraY)))
+    #left end
+    #right
+    if cameraX <= -60:
+        pygame.draw.polygon(win, (255, 0, 0),
+                            ((300 + cameraX, 100 + cameraY),
+                            (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (300 + cameraX, 300 + cameraY)))
+        pygame.draw.polygon(win, (0, 0, 255),
+                            ((300 + cameraX, 300 + cameraY),
+                            (280 + (cameraX / 1.5), 120 + (cameraY / 1.5)),
+                            (280 + (cameraX / 1.5), 280 + (cameraY / 1.5))))
+    #right end
+    #front
+    pygame.draw.polygon(win, (255, 0, 0),
+                        ((100 + cameraX, 100 + cameraY),
+                         (300 + cameraX, 100 + cameraY),
+                         (100 + cameraX, 300 + cameraY)))
+    pygame.draw.polygon(win, (0, 0, 255),
+                        ((100 + cameraX, 300 + cameraY),
+                         (300 + cameraX, 100 + cameraY),
+                         (300 + cameraX, 300 + cameraY)))
+    #front end
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
