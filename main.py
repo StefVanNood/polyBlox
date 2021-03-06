a0717e9a11a8d8f412aa1e50ca945d7ad67abf9f
StefVanNood <vannoodstef@gmail.com>
2021-03-06 14:51:19 +0100

    pygame7

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ y_screen = 500
 win = pygame.display.set_mode((x_screen, y_screen))
 pygame.display.set_caption("doodle")
 
+cameraX = 0
+cameraY = 0
+vel = 5
+
 run = True
 while run:
     win.fill((0, 0, 0))
     pygame.time.delay(16)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        cameraX = cameraX - vel
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        cameraX = cameraX + vel
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        cameraY = cameraY - vel
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        cameraY = cameraY + vel
+
     pygame.draw.line(win, (255, 255, 255), (100, 100), (300, 100))
     pygame.draw.line(win, (255, 255, 255), (100, 100), (100, 300))
     pygame.draw.line(win, (255, 255, 255), (100, 300), (300, 300))
@@ -25,6 +40,7 @@ while run:
     pygame.draw.line(win, (255, 255, 255), (120, 120), (120, 280))
     pygame.draw.line(win, (255, 255, 255), (120, 280), (280, 280))
     pygame.draw.line(win, (255, 255, 255), (280, 280), (280, 120))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
