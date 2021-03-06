d44a34467e849476f7cfe5661b98b9f1fee416fe
StefVanNood <vannoodstef@gmail.com>
2021-03-06 14:48:45 +0100

    pygame6

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ while run:
     pygame.draw.line(win, (255, 255, 255), (100, 100), (100, 300))
     pygame.draw.line(win, (255, 255, 255), (100, 300), (300, 300))
     pygame.draw.line(win, (255, 255, 255), (300, 300), (300, 100))
+
+    pygame.draw.line(win, (255, 255, 255), (100, 100), (120, 120))
+    pygame.draw.line(win, (255, 255, 255), (100, 300), (120, 280))
+    pygame.draw.line(win, (255, 255, 255), (300, 300), (280, 280))
+    pygame.draw.line(win, (255, 255, 255), (300, 100), (280, 120))
+
+    pygame.draw.line(win, (255, 255, 255), (120, 120), (280, 120))
+    pygame.draw.line(win, (255, 255, 255), (120, 120), (120, 280))
+    pygame.draw.line(win, (255, 255, 255), (120, 280), (280, 280))
+    pygame.draw.line(win, (255, 255, 255), (280, 280), (280, 120))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
