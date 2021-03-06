40edc33d9e847964ba24121523272bced66bd15e
StefVanNood <vannoodstef@gmail.com>
2021-03-06 13:41:18 +0100

    pygame1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pygame
-print("this was edited by stef")
+pygame.init()
+
+x_screen = 500
+y_screen = 500
+
+win = pygame.display.set_mode((x_screen, y_screen))
+pygame.display.set_caption("ScrapCraft")
+
+print("test")
