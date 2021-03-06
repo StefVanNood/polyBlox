054b832a204fa82f78b01af4c82c57c686baada4
StefVanNood <vannoodstef@gmail.com>
2021-03-06 14:26:43 +0100

    pygame4

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("this was edited by stef and jordi.")
+import pygame
+pygame.init()
+
+x_screen = 500
+y_screen = 500
+
+win = pygame.display.set_mode((x_screen, y_screen))
+pygame.display.set_caption("doodle")
+
+run = True
+while run:
+    win.fill((0, 0, 0))
+    pygame.time.delay(16)
+    pygame.draw.line(win, (255, 255, 255), (60, 80), (200, 200))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+
+pygame.quit()
+print("done")
