57dad452da0151d01d7299f4350f652b60af9b54
StefVanNood <vannoodstef@gmail.com>
2021-03-06 14:39:15 +0100

    pygame5

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ run = True
 while run:
     win.fill((0, 0, 0))
     pygame.time.delay(16)
-    pygame.draw.line(win, (255, 255, 255), (60, 80), (200, 200))
+    pygame.draw.line(win, (255, 255, 255), (100, 100), (300, 100))
+    pygame.draw.line(win, (255, 255, 255), (100, 100), (100, 300))
+    pygame.draw.line(win, (255, 255, 255), (100, 300), (300, 300))
+    pygame.draw.line(win, (255, 255, 255), (300, 300), (300, 100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     pygame.display.update()
-
 pygame.quit()
 print("done")
